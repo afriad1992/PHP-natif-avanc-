aee9d35a33453c1f60716f68e1cf72b5f8f82415
afriad1992 <56979262+afriad1992@users.noreply.github.com>
2020-03-05 10:48:49 +0100

    Add files via upload

---
+++ email.php
@@ -0,0 +1,21 @@
+﻿<?php
+
+	
+    mail('ghizlane.afriad@gmail.com',
+	
+         'Confirmation d\'inscription',
+	
+         'Bienvenue sur Jarditou ! Tu peux y acheter des tomates cerises pour l\'apéro et une brouette pour les transporter. Sors vite ton American Express !',
+	
+         array('MIME-Version' => '1.0',
+               'Content-Type' => 'text/html; charset=utf-8',
+             'From' => 'elqerouani@gmail.com',
+	
+        
+	
+                'X-Mailer' => 'PHP/' . phpversion()
+	
+                )
+	
+        );
+	?>
